17a74a3dfecc7668a42830b24d21060d0411f9a8
DFectuoso <santiago1717@gmail.com>
2011-09-17 18:13:54 -0400

    Adding foursquare login, no keys btw kk thanks

--- main.py
+++ main.py
@@ -1,33 +1,48 @@
-#!/usr/bin/env python
-#
-# Copyright 2007 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp import util
 
+from foursquare import *                                                      
+from google.appengine.api import urlfetch
+import logging  
+import keys
+
+from django.utils import simplejson as json 
+
+helper = FoursquareAuthHelper(keys.foursquare_client, keys.foursquare_secret, "http://127.0.0.1:9119/oauth_callback")
 
 class MainHandler(webapp.RequestHandler):
-    def get(self):
-        self.response.out.write('Hello world!')
+  def get(self):
+    self.response.out.write('Hello world!')
 
+class LoginHandler(webapp.RequestHandler):
+  def get(self):
+    self.redirect(helper.get_authentication_url())
+
+class OAuthCallbackHandler(webapp.RequestHandler):
+  def get(self):
+    code = self.request.get("code")                                           
+    token_url = helper.get_access_token_url(code)
+    result = urlfetch.fetch(token_url)                                        
+    result = json.loads(result.content)
+
+    #### TODO: LOG THE USER IN(OUR SESSION)
+    #### TODO: IF THE USER DOESN'T EXIST, STORE IN THE DB
+    #### TODO: IF IT DOES EXIST, WE UPDATE THE ACCESS TOKEN
+    #client = FoursquareClient(result["access_token"])                         
+    #lol = client.users_venuehistory()
+
+    self.redirect('/')
+
+
+    self.response.out.write('Hello world!')
 
 def main():
-    application = webapp.WSGIApplication([('/', MainHandler)],
-                                         debug=True)
-    util.run_wsgi_app(application)
-
+  application = webapp.WSGIApplication([
+    ('/', MainHandler),
+    ('/login', LoginHandler),
+    ('/oauth_callback', OAuthCallbackHandler),
+  ], debug=True)
+  util.run_wsgi_app(application)
 
 if __name__ == '__main__':
-    main()
+  main()
