ae621edf388b4aa5a6fe7815a8c55ed2a04bbaed
DFectuoso <santiago1717@gmail.com>
2011-09-17 19:14:53 -0400

    User object yey

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from google.appengine.ext import webapp
+from google.appengine.ext import webapp, db
 from google.appengine.ext.webapp import util, template
 
 from foursquare import *
@@ -11,20 +11,49 @@ from gaesessions import get_current_session
 
 helper = FoursquareAuthHelper(keys.foursquare_client, keys.foursquare_secret, keys.foursquare_callback)
 
+class User(db.Model):
+  access_token  = db.StringProperty(required=False)
+  foursquare_id = db.StringProperty(required=False)
+  first_name    = db.StringProperty(required=False)
+  last_name     = db.StringProperty(required=False)
+
+  @staticmethod
+  def get_or_create_user_by_foursquare_id(id):
+    user_query = User.all().filter("foursquare_id", id).fetch(1)
+    if len(user_query) == 0:
+      user = User(foursquare_id=id)
+      user.put()
+    else:
+      user = user_query[0]
+    return user
+
 class MainHandler(webapp.RequestHandler):
   def get(self):
-    self.response.out.write(template.render('templates/main.html', locals()))
+    session = get_current_session()
+    if session.has_key('user'):
+      self.redirect("/dashboard")
+    else:
+      self.response.out.write(template.render('templates/main.html', locals()))
 
 class DashboardHandler(webapp.RequestHandler):
   def get(self): 
     session = get_current_session()
-    if session.has_key('access_token'):
-      access_token = session['access_token']
-    self.response.out.write(template.render('templates/dashboard.html', locals()))
+    if session.has_key('user'):
+      self.response.out.write(template.render('templates/dashboard.html', locals()))
+    else:
+      self.redirect("/")
 
 class LoginHandler(webapp.RequestHandler):
   def get(self):
     self.redirect(helper.get_authentication_url())
+
+class LogoutHandler(webapp.RequestHandler):
+  def get(self):
+    session = get_current_session()
+    if session.is_active():
+      session.terminate()
+    session.regenerate_id()
+    self.redirect('/')
 
 class OAuthCallbackHandler(webapp.RequestHandler):
   def get(self):
@@ -33,19 +62,19 @@ class OAuthCallbackHandler(webapp.RequestHandler):
     result = urlfetch.fetch(token_url)                                        
     result = json.loads(result.content)
 
+    client = FoursquareClient(result["access_token"])                         
+    user_info = client.users()
+    user = User.get_or_create_user_by_foursquare_id(user_info["response"]["user"]["id"])
+    user.first_name = user_info["response"]["user"]["firstName"]
+    user.last_name  = user_info["response"]["user"]["lastName"]
+    user.access_token = result["access_token"] 
+    user.put()
+
     session = get_current_session()
     if session.is_active():
       session.terminate()
     session.regenerate_id()
-    session['access_token'] =  result["access_token"]
-
-
-    #### TODO: LOG THE USER IN(OUR SESSION)
-    #### TODO: IF THE USER DOESN'T EXIST, STORE IN THE DB
-    #### TODO: IF IT DOES EXIST, WE UPDATE THE ACCESS TOKEN
-    #client = FoursquareClient(result["access_token"])                         
-    #lol = client.users_venuehistory()
-
+    session['user'] = user 
     self.redirect('/dashboard')
 
 def main():
@@ -53,6 +82,7 @@ def main():
     ('/', MainHandler),
     ('/dashboard', DashboardHandler),
     ('/login', LoginHandler),
+    ('/logout', LogoutHandler),
     ('/oauth_callback', OAuthCallbackHandler),
   ], debug=True)
   util.run_wsgi_app(application)
